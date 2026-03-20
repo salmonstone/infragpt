ea6941a68da1dc53a9828e60f17795934937a15c
Your Name <you@example.com>
2026-03-20 07:02:11 +0000

    fix: add domain infragpt.online

---
+++ script5.sh
@@ -0,0 +1,3 @@
+sed -i "s/YOUR_DOMAIN.com/infragpt.online/g" k8s/ingress.yaml
+sed -i "s/YOUR_DOMAIN.com/infragpt.online/g" elk/03-kibana.yaml
+sed -i "s/YOUR_DOMAIN.com/infragpt.online/g" helm/infragpt/values.yaml
